d97390b05f6823c4c2aeea31a1b7ce0500e7176d
Bindajoba <70373972+Bindajoba@users.noreply.github.com>
2023-06-06 11:20:18 +0200

    Fixed Tags

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '336736248974774662147214658495532931078'                       // used to check whether the localStorage data is stale
+var gzip_hash = '180291895294598370068724240242048037738'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
