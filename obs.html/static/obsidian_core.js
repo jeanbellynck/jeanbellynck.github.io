14e883f79590fddb62f752d3f4bfc5fad0887f59
Jean-Baptiste Bellynck <bellynck.jean@gmail.com>
2023-11-16 01:45:33 +0900

    Blog 6.1

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '43410673381146253931385735173677364620'                       // used to check whether the localStorage data is stale
+var gzip_hash = '253189205479697706737845599320982405295'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
