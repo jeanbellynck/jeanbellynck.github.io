34dab2e9c36e7e90a81511a1421cffbcab2df53e
Jean-Baptiste Bellynck <bellynck.jean@gmail.com>
2023-12-08 11:01:56 +0900

    Improved Bibliography visibility

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '31195948363242524048095266826614034624'                       // used to check whether the localStorage data is stale
+var gzip_hash = '323144003775527116145766530275625798372'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
